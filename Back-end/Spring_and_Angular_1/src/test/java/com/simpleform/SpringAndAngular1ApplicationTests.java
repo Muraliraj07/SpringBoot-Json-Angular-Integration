d5870ff3aece9d14bc899513ff5d3efe162e66ce
murali <muraliraj9801@gmail.com>
2023-06-15 10:40:29 +0530

    Initial commit

---
+++ Back-end/Spring_and_Angular_1/src/test/java/com/simpleform/SpringAndAngular1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simpleform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAndAngular1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
